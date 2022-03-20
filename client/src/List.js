2b16f8bf578b334e455c05e78e7e87c295d32f2f
Mitch Faber <faberskits@gmail.com>
2022-03-20 13:41:28 -0300

    Adding DateRange.

--- client/src/List.js
+++ client/src/List.js
@@ -2,17 +2,29 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import DateRange from "./DateRange";
 
 // This exports the whole icon packs for Brand and Solid.
 library.add(fas);
 
 export default function List() {
-	const [selectedRecID, setSelectedRecID] = useState("1");
-	const [selectedRecText, setselectedRecText] = useState("1");
+	const [selectedRecID, setSelectedRecID] = useState("nothing");
+	const [selectedRecText, setselectedRecText] = useState("nothing");
+	const [input, setInput] = useState("");
+
 	function changedRec(e) {
 		setSelectedRecID(e.target.value);
-		setselectedRecText(e.target[e.target.value - 1].text);
+		if (e.target.value !== "nothing") {
+			setselectedRecText(e.target[e.target.value].text);
+		} else {
+			setselectedRecText("nothing");
+		}
 	}
+
+	function addIngredient() {
+		setInput("");
+	}
+
 	function addRecipe() {
 		console.log(selectedRecText);
 	}
@@ -26,6 +38,7 @@ export default function List() {
 							value={selectedRecID}
 							className="btn btn-outline-secondary "
 							aria-label="Default select example">
+							<option value="nothing">Select a recipe to add</option>
 							<option value="1">Chicken and Potatoes</option>
 							<option value="2">Tacos</option>
 							<option value="3">Chicken Alfredo</option>
@@ -39,6 +52,7 @@ export default function List() {
 							</span>
 						</div>
 					</div>
+
 					<table className="table">
 						<thead>
 							<tr>
@@ -63,79 +77,78 @@ export default function List() {
 					</table>
 				</div>
 				<div className="col-12 col-md-6">
-					<form>
-						<div className="mt-3 mb-3 input-group">
-							<input className="form-control" type="text" />
-							<div className="input-group-append">
-								<span className="input-group-text">
-									<button href="#" className="btn btn-link text-secondary">
-										<FontAwesomeIcon icon={["fas", "plus"]} />
-									</button>
-								</span>
-							</div>
+					<div className="mt-3 mb-3 input-group">
+						<input className="form-control" type="text" value={input} onInput={(e) => setInput(e.target.value)} />
+						<div className="input-group-append">
+							<span className="input-group-text">
+								<button onClick={addIngredient} className="btn btn-link text-secondary">
+									<FontAwesomeIcon icon={["fas", "plus"]} />
+								</button>
+							</span>
 						</div>
-						<div style={{ overflow: "auto", height: "500px" }}>
-							<table className="table">
-								<thead>
-									<tr>
-										<th scope="col">Item Name</th>
-										<th scope="col">Recipe</th>
-										<th scope="col">Remove</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<td>Chicken</td>
-										<td>Butter Chicken</td>
-										<td>
-											<button href="#" className="btn btn-link text-danger">
-												<FontAwesomeIcon icon={["fas", "minus-circle"]} />
-											</button>
-										</td>
-									</tr>
-									<tr>
-										<td>Chicken</td>
-										<td>Butter Chicken</td>
-										<td>
-											<button href="#" className="btn btn-link text-danger">
-												<FontAwesomeIcon icon={["fas", "minus-circle"]} />
-											</button>
-										</td>
-									</tr>
-									<tr>
-										<td>Chicken</td>
-										<td>Butter Chicken</td>
-										<td>
-											<button href="#" className="btn btn-link text-danger">
-												<FontAwesomeIcon icon={["fas", "minus-circle"]} />
-											</button>
-										</td>
-									</tr>
-									<tr>
-										<td>Chicken</td>
-										<td>Butter Chicken</td>
-										<td>
-											<button href="#" className="btn btn-link text-danger">
-												<FontAwesomeIcon icon={["fas", "minus-circle"]} />
-											</button>
-										</td>
-									</tr>
-									<tr>
-										<td>Chicken</td>
-										<td>Butter Chicken</td>
-										<td>
-											<button href="#" className="btn btn-link text-danger">
-												<FontAwesomeIcon icon={["fas", "minus-circle"]} />
-											</button>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-						<button className="btn btn-primary" type="submit">
-							Submit
-						</button>
-					</form>
+					</div>
+					<div style={{ overflow: "auto", height: "500px" }}>
+						<table className="table">
+							<thead>
+								<tr>
+									<th scope="col">Item Name</th>
+									<th scope="col">Recipe</th>
+									<th scope="col">Remove</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Chicken</td>
+									<td>Butter Chicken</td>
+									<td>
+										<button href="#" className="btn btn-link text-danger">
+											<FontAwesomeIcon icon={["fas", "minus-circle"]} />
+										</button>
+									</td>
+								</tr>
+								<tr>
+									<td>Chicken</td>
+									<td>Butter Chicken</td>
+									<td>
+										<button href="#" className="btn btn-link text-danger">
+											<FontAwesomeIcon icon={["fas", "minus-circle"]} />
+										</button>
+									</td>
+								</tr>
+								<tr>
+									<td>Chicken</td>
+									<td>Butter Chicken</td>
+									<td>
+										<button href="#" className="btn btn-link text-danger">
+											<FontAwesomeIcon icon={["fas", "minus-circle"]} />
+										</button>
+									</td>
+								</tr>
+								<tr>
+									<td>Chicken</td>
+									<td>Butter Chicken</td>
+									<td>
+										<button href="#" className="btn btn-link text-danger">
+											<FontAwesomeIcon icon={["fas", "minus-circle"]} />
+										</button>
+									</td>
+								</tr>
+								<tr>
+									<td>Chicken</td>
+									<td>Butter Chicken</td>
+									<td>
+										<button href="#" className="btn btn-link text-danger">
+											<FontAwesomeIcon icon={["fas", "minus-circle"]} />
+										</button>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<DateRange></DateRange>
+					<button className="mb-3 btn btn-primary" type="submit">
+						Submit
+					</button>
 				</div>
 			</div>
 		</div>
