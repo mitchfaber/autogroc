6336325d0df530170b7664ed76df1368c4cec30b
Mitch Faber <faberskits@gmail.com>
2022-05-18 07:43:15 -0300

    - added regular icons to checklist. - Found circle to use for check button. - formatting row for checklist, in progress.

--- client/src/CheckList.js
+++ client/src/CheckList.js
@@ -5,23 +5,51 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import IngredientTable from "./IngredientTable";
 import { v4 as uuidv4 } from "uuid";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 export default function CheckList() {
 	let { id } = useParams();
 	const [loading, setLoading] = useState(true);
+	const [ingredients, setIngredients] = useState([]);
+	library.add(far);
+	library.add(fas);
+
 	function getPlan() {
 		fetch(`http://localhost:8080/plan/${id}`)
 			.then((res) => res.json())
 			.then((result) => {
-				setLoading(false);
 				console.log(result);
+				setIngredients(result.ingredients);
+				setLoading(false);
 			});
 	}
+
 	useEffect(() => {
 		getPlan();
 	}, []);
 	if (loading === false) {
-		return <div className="container">CheckList</div>;
+		return (
+			<div className="container">
+				<div className="row">
+					<table>
+						<tbody>
+							{ingredients.map((ing) => {
+								return (
+									<tr key={uuidv4()}>
+										<td>
+											<button class="btn">
+												<FontAwesomeIcon icon={["far", "circle"]} />
+											</button>
+										</td>
+										<td>{ing.name}</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		);
 	} else {
 		return <div className="container">Loading...</div>;
 	}
