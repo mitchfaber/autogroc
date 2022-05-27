47ca423d5ddf081e8cbbe6cc110c8c6bf93c2d63
Mitch Faber <faberskits@gmail.com>
2022-05-27 13:24:57 -0300

    Added "are you sure" modal for deleting plans that are not yet complete

--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -10,6 +10,7 @@ library.add(fas);
 export default function Homepage() {
 	const [loading, setLoading] = useState(false);
 	const [plans, setPlans] = useState();
+	let id;
 	useEffect(() => {
 		getPlans();
 	}, []);
@@ -35,6 +36,39 @@ export default function Homepage() {
 	} else {
 		return (
 			<div className="container">
+				<div
+					className="modal fade"
+					id="exampleModal"
+					tabIndex="-1"
+					role="dialog"
+					aria-labelledby="exampleModalLabel"
+					aria-hidden="true">
+					<div className="modal-dialog" role="document">
+						<div className="modal-content">
+							<div className="modal-header">
+								<h5 className="modal-title" id="exampleModalLabel">
+									Delete Meal plan?
+								</h5>
+								<button type="button" className="close" data-dismiss="modal" aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</div>
+							<div className="modal-body">Are you sure you would like to delete the incomplete plan?</div>
+							<div className="modal-footer">
+								<button type="button" className="btn btn-secondary" data-dismiss="modal">
+									Close
+								</button>
+								<button
+									type="button"
+									onClick={() => deletePlan(id)}
+									data-dismiss="modal"
+									className="btn btn-danger">
+									Delete
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div className="row mt-5">
 					<div className="col-6">
 						<div>
@@ -67,9 +101,21 @@ export default function Homepage() {
 														</Link>
 													</td>
 													<td>
-														<button onClick={() => deletePlan(plan._id)} className="btn btn-danger">
-															<FontAwesomeIcon icon={["fas", "trash"]} />
-														</button>
+														{plan.complete ? (
+															<button
+																className="btn btn-danger"
+																onClick={() => deletePlan(plan._id)}>
+																<FontAwesomeIcon icon={["fas", "trash"]} />
+															</button>
+														) : (
+															<button
+																data-toggle="modal"
+																data-target="#exampleModal"
+																className="btn btn-danger"
+																onClick={() => (id = plan._id)}>
+																<FontAwesomeIcon icon={["fas", "trash"]} />
+															</button>
+														)}
 													</td>
 												</tr>
 											);
